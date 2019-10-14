33f696799505f64d3036625e4480dfacd7e1a9bd
Lisa <lisa.michael@hotmail.com>
2019-10-14 14:40:03 -0400

    created object with question and answer. only tho

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,21 @@
+
+// declare my variables 
+let wins= 0;
+let losses=0;
+let unanswered=0; 
+let option1 = "";
+let option2 = "";
+let option3 = "";
+let option4 = "";
+
+
+//create an object to store my questions and answers
+
 let qNa = {
-    question:  "",
-    answer: '',
-    option1: "",
-    option2: "",
-    option3: "",
-    option4: "",
-    giphy: ""
+    question:  "what is my first question",
+    answer: 'my answer is an array element',
+    
+    // place users possible guesses in an array 
+    userGuess: [option1,option2,option3,option4, answerId, AnswerText], 
+    giphy: "https://giv"
 }
