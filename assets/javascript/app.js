349ad6cf323950de17d74ad665f8d012fda75fcc
Lisa <lisa.michael@hotmail.com>
2019-10-15 09:14:18 -0400

    working on h2 w/timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,71 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-// declare my variables 
-let wins= 0;
-let losses=0;
-let unanswered=0; 
-let option1 = "";
-let option2 = "";
-let option3 = "";
-let option4 = "";
+    // declare my variables 
+    let wins = 0;
+    let losses = 0;
+    let unanswered = 0;
+    let option1 = "";
+    let option2 = "";
+    let option3 = "";
+    let option4 = "";
+    let answerId = 0;
+    let answerText = "";
+    let clockRunning = false;
+    let time = 30;
+    let intervalId;
 
 
-//create an object to store my questions and answers
 
-let qNa = {
-    question:  "what is my first question",
-    answer: 'my answer is an array element',
-    
-    // place users possible guesses in an array 
-    userGuess: [option1,option2,option3,option4, answerId, AnswerText], 
-    giphy: "https://giv"
-},
+    if (!clockRunning) {
+        intervalId = setInterval(count, 1000);
+        clockRunning = true;
+    }
+    else { !clockRunning }
 
-function startButton(){
-    alert("startbutton");
-}
 
-function playGame() {
-    alert("playGame");
-}
-startButton();
-playGame();
-}) // end of $(document).ready 
+
+    function count() {
+
+
+        // DONE: increment time by 1, remember we cant use "this" here.
+        time--;
+
+        // DONE: Get the current time, pass that into the timeConverter function,
+        //       and save the result in a variable.
+        // const converted = timeConverter(time);
+        console.log(time);
+
+        // DONE: Use the variable we just created to show the converted time in the "display" div.
+        $("#timerPart2").html(time + " seconds");
+    }
+
+
+
+
+    //create an object to store my questions and answers
+
+    let qNa = {
+        question: "what is my first question",
+        answer: 'my answer is an array element',
+
+        // place users possible guesses in an array 
+        userGuess: [option1, option2, option3, option4, answerId, answerText],
+        giphy: "https://giv"
+    }
+
+    function startButton() {
+        alert("startbutton");
+        count();
+    }
+
+    function playGame() {
+        alert("playGame");
+    }
+
+    startButton();
+    // playGame();
+
+
+})
+// end of $(document).ready
