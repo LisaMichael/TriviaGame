b8f661415aaf4576b5a43b5ec16c78523caa71b6
Lisa <lisa.michael@hotmail.com>
2019-11-10 23:19:06 -0500

    fixed app.js. incorrect questions somewhat working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,10 +8,8 @@ $(document).ready(function () {
     let losses = 0;
     let unanswered = 0;
     // let question = 0;
-    // let answerId ;
     // let answerText = "";
     let clockRunning = false;
-    // let time = 30;
 
     // set timer =5 for testing
     let time = 30;
@@ -58,17 +56,26 @@ $(document).ready(function () {
             //   question: 3,
             answerId: 2,
             question: "What did Dilbert name his dog ? ",
+            answer: "Dogbert",
             userGuess: ["DogPile", "Blog the Dog", "Dogbert", "FiFo"],
             giphy: "./assets/images/question3.gif",
         },
         {
             //question4
-            answerId: 3,
+            answerId: 2,
             question: "First couple to be televised tv in bed together ?",
+            answer: "Fred & Wilma Flintstone",
             userGuess: ["George & Jane Jetson", "Pebbles & Bam Bam", "Fred & Wilma Flintstone", "Mickey Mouse & Minnie Mouse"],
             giphy: "./assets/images/question4.gif",
+        },
+        {
+            //question5 
+            answerId: 0,
+            question: "What year was the Jetsons set in?",
+            answer: "2062",
+            userGuess: ["2062", "2099", "2100", "2222"],
+            giphy: "./assets/images/question5.gif"
         }
-
     ];
 
 
@@ -89,15 +96,20 @@ $(document).ready(function () {
         // unhide gametime button
         $("#displayCorrect").css("display", "none");
 
+        // empty the previous image 
+        
         // Once all questions are answered, display score results 
-        if (questionSet > questionArray.length - 1) {
-            console.log("line 90 ... i need the scoreboard");
-            timerIsRunning();
+        if (questionSet > questionArray.length) {
+            console.log("line 93 ... i need the scoreboard");
+            // timerIsRunning();
             scoreboard();
         }
         else {
-            if (questionSet < questionArray.length) {
 
+            //if the question # is less than the length of array
+            if (questionSet < questionArray.length) {
+                time = 30;
+                console.log("line 99: questionSet <questionArray.length")
                 //created a div to display question property in questionArray[]
                 let questionToAnswer = $("<div>");
                 questionToAnswer.addClass("questionCSS");
@@ -111,49 +123,56 @@ $(document).ready(function () {
                 // usedhttps://www.javatpoint.com/jquery-addclass to assist  
                 // also wk 6 customer-object exercise assisted me with the array
 
+                //create a div for each current question called questionCurrent
                 for (i = 0; i < 4; i++) {
                     const currentQuestion = $('<div>' + questionArray[questionSet].userGuess[i] + '</div>');
 
                     // need to add class to take advantage of bootstrap
-                    // currentQuestion.addClass('data-mask flex-center', i);
+
                     currentQuestion.addClass("questionCurrent");
                     currentQuestion.addClass("hoverdiv");
                     // referenced for attr info  https://www.w3schools.com/jquery/html_attr.asp
                     currentQuestion.attr("data-index", i);
                     questionlist.append(currentQuestion);
 
-                }
+                } //end of for loop
 
                 // code for on click when all 4 choices are displayed
 
                 $('.questionCurrent').on("click", function () {
                     if (time < 0) {
                         console.log("times up")
+                        $('#imageCorrect').empty();
+                        $('#imageCorrect').append('<img src ="./assets/images/question5.gif">');
                         emptyQA();
                         unanswered++;
-                        questionSet++;
+                        // questionSet++;
                         clearInterval(intervalId);
-                        displayQuestion();
+                        // displayQuestion();
+                        scoreboard();
                     }
                     else {
 
                         // working on this part of the code now
                         //extract the value from the div i clicked on
-                        console.log("text inside line 114 ");
+                        console.log("text inside line 141 ");
                         indexValue = ($(this).attr("data-index"));
 
                         // select answerID based upon what index value is
                         // if the values are the same, the answer is correct 
 
                         if (indexValue == (questionArray[questionSet].answerId)) {
+                            console.log("indexValue on line 148: " + indexValue);
 
                             correct();
+
                         }
 
                         // if the answers are not the same, then the answer is incorrect
-                        else {
 
-                            console.log("this is line 143 - wrong answer");
+                        else {
+                            indexValue != (questionArray[questionSet].answerId)
+                            console.log("this is line 155 - wrong answer");
                             wrong();
                         }
 
@@ -169,11 +188,14 @@ $(document).ready(function () {
     } //end of displayQuestion function
 
     function scoreboard() {
-        $('#imageCorrect').empty();
+    
+
+ $('#imageCorrect').empty();
         emptyQA();
-        alert("scoreboard window");
-        playAgain();
-        time = 3;
+        let tally = $("<div>");
+        tally.html("<p>Here's how you did:</p>")
+        $("#possibleAnswers").append(tally);
+       
         clearInterval(intervalId);
 
 
@@ -186,29 +208,35 @@ $(document).ready(function () {
 
         $("#possibleAnswers").append(scoreLosses);
         $("#possibleAnswers").append(scoreWin);
+        playAgain();
         // }
 
-    }
+    } // end of scoreboard function
 
     function correct() {
 
-        let urCorrect = $('<div>');
-        $("#possibleAnswers").html('<h3> You are absolutely CORRECT !</h3>');
+        let urCorrect = $("<div>");
+        urCorrect.text("you are absolutely correct")
+        $("#possibleAnswers").append(urCorrect);
+        console.log("you are absolutely correct");
         // if question is answered correctly , display correct giphy 
         //add image for correct answer: 
         correctImage();
 
-        clearInterval(intervalId);
+        // clearInterval(intervalId);
         clockRunning = false;
-        time = 30;
+        time = 5;
         questionSet++;
+        console.log("The question set is now: " + questionSet);
         emptyQA();
         displayQuestion();
-
 
         count();
         wins++;
 
+        //     if (questionSet > questionArray.length) {
+        //         scoreboard();
+        //     }
 
     }
 
@@ -226,71 +254,117 @@ $(document).ready(function () {
 
 
         if (questionSet === 0) {
+            console.log("test text0");
             $('#imageCorrect').empty();
-            $('#imageCorrect').append('<img src="./assets/images/question0.gif" height="200px" width="200px">');
+ $('#imageCorrect').append('<img src="./assets/images/question0.gif" height="200px" width="200px">');
         } if (questionSet == 1) {
+            console.log("test text1");
             $('#imageCorrect').empty();
-            $('#imageCorrect').append('<img src ="./assets/images/question1.gif">');
+            $('#imageCorrect').append('<img src ="./assets/images/question1.gif"height="200px" width="200px">');
         }
         if (questionSet == 2) {
+            console.log("test text2");
             $('#imageCorrect').empty();
             $('#imageCorrect').append('<img src ="./assets/images/question2.gif">');
         } if (questionSet == 3) {
+            console.log("test text3");
             $('#imageCorrect').empty();
             $('#imageCorrect').append('<img src ="./assets/images/question3.gif">');
         }
+        if (questionSet == 4) {
+            console.log("test text4");
+            $('#imageCorrect').empty();
+            $('#imageCorrect').append('<img src ="./assets/images/question4.gif">');
+        }
+        if (questionSet == 5) {
+            console.log("test text5");
+            $('#imageCorrect').empty();
+            $('#imageCorrect').append('<img src ="./assets/images/question5.gif">');
+        }
     }
+
+ function incorrectImage() {
+
+
+        if (questionSet === 0) {
+            console.log("test incorrect0");
+            $('#imageCorrect').empty();
+            $('#imageCorrect').append('<img src="./assets/images/question0.gif" height="200px" width="200px">');
+        } if (questionSet == 1) {
+            console.log("test incorrect1");
+            $('#imageCorrect').empty();
+            $('#imageCorrect').append('<img src ="./assets/images/question0.gif">');
+        }
+        if (questionSet == 2) {
+            console.log("test incorrect2");
+            $('#imageCorrect').empty();
+            $('#imageCorrect').append('<img src ="./assets/images/question1.gif">');
+        } if (questionSet == 3) {
+            console.log("test incorrect3");
+            $('#imageCorrect').empty();
+            $('#imageCorrect').append('<img src ="./assets/images/question2.gif">');
+        }
+        if (questionSet == 4) {
+            console.log("test incorrect4");
+            $('#imageCorrect').empty();
+            $('#imageCorrect').append('<img src ="./assets/images/question3.gif">');
+        }
+        if (questionSet == 5) {
+            console.log("test incorrect5");
+            $('#imageCorrect').empty();
+            $('#imageCorrect').append('<img src ="./assets/images/question4.gif">');
+        }
+    }
+
 
     function wrong() {
         console.log("wrong answer");
+
+        // empty previous questions
         emptyQA();
         losses++;
         // count();
-        time = 30;
+        time = 5;
 
-        clockRunning = false;
-        timerIsRunning();
+        // clockRunning = false;
 
-        displayQuestion();
+        // displayQuestion();
 
-        console.log("line 200 " + questionSet);
-
+        console.log("line 259. This is the question set " + questionSet);
         //if the questionSet Variable is more than the length of the questionArray
-        // we should go to the scoreboard
+        // we should go to the scoreboard (we are at the last question)
         if (questionSet >= questionArray.length) {
+            console.log("if (questionSet > questionArray.length) part of code")
+            console.log("line 262. This is the question set " + questionSet);
+            losses++
+            clearInterval(intervalId);
+            time = 0;
             scoreboard();
         } else {
+            
             // display the correct answer if wrong answer selected
+
+            // displays the correct answer 
             let correctDisplay = questionArray[questionSet].answer;
-
+            // displayQuestion();
             // count();
-
+            console.log("The correct answer is: " + correctDisplay);
+            console.log("The question set is: " + questionSet)
             // unhide displayCorrect
             $("#displayCorrect").css("display", "inline-block");
 
             //  Used the span to "combine" id gamertimer defined in html.
             $("#displayQues").html(correctDisplay);
-
-
-
-            // console.log(correctDisplay);
+            time = 5;
+            questionSet++;
 
             // $("#possibleAnswers").html("<h3>The correct answer is: " + correctDisplay + "</h3>");
 
-
-
             // display correct gif
-            correctImage();
+            incorrectImage();
 
-            // count();
         }
     }
-
-
-
-
-    
-
 
 
     // function to start countdown timer 
@@ -301,16 +375,14 @@ $(document).ready(function () {
             //set intervalId = 1 sec
             intervalId = setInterval(count, 1000);
             clockRunning = true;
-
         }
 
         // Once all questions are answered, display score results 
-        if (questionSet > questionArray.length) {
+        // if (questionSet > questionArray.length) {
+        //     console.log("am i hitting this code? ")
+        //     scoreboard();
+        // }
 
-            console.log("am i hitting this code? ")
-            scoreboard();
-        }
-    
 
     }
 
@@ -329,7 +401,6 @@ $(document).ready(function () {
 
         if (time >= 0) {
 
-
             // const converted = timeConverter(time);
             console.log(time);
 
@@ -342,20 +413,24 @@ $(document).ready(function () {
         }
 
         //length of array is 4 , this code is less than the length of array 
-        
 
+        // if (time < 0 && questionSet === questionArray.length){
+        //     console.log("questionSet = questionArray.length");
+        //     clearInterval(intervalId);
+        //     scoreboard();
+        // }
 
-        if (time < 0 && questionSet < questionArray.length - 1) {
+        if (time < 0 && questionSet <= questionArray.length) {
             console.log("question set = " + questionSet);
             //increment question set to move onto the next question
-            questionSet++;
+            // questionSet++;
             console.log("question set NOW = " + questionSet);
             // empty existing question and possible answers
             // $("#questions").empty();
             // $("#possibleAnswers").empty();
+            wrong();
             emptyQA();
-
-            timerIsRunning();
+            // timerIsRunning();
             //display next question
             displayQuestion();
 
@@ -363,7 +438,7 @@ $(document).ready(function () {
         }
     }
 
-// function to display start button
+    // function to display start button
 
     function startButton() {
 
@@ -411,7 +486,8 @@ $(document).ready(function () {
 
     function playAgain() {
         alert("playGame");
-
+        $("#playMore").empty();
+        // clearInterval(intervalId);
         let playButton = $("<button>");
 
 
@@ -435,15 +511,16 @@ $(document).ready(function () {
             $("#gameTimer").css("display", "inline-block");
             // hide the button after you click on it
             $(".playProperties").css("display", "none");
-
+            time = 30;
+            questionSet = 0;
             clockRunning = false;
             timerIsRunning();
             count();
             emptyQA();
-            questionSet = 0;
+
             wins = 0;
             losses = 0;
-            clearInterval(intervalId);
+            // clearInterval(intervalId);
             displayQuestion();
         })
     }
